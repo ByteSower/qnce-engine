b349db140ec401f348b033a0ce38e3af8a82dc70
RashanH <thebullball@icloud.com>
2025-08-15 20:00:14 -0400

    chore(lint): convert ESLint config to CommonJS to fix __esModule warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,30 @@
-export default [
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: 'module',
-        project: './tsconfig.json'
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/prefer-const': 'error',
+        '@typescript-eslint/no-inferrable-types': 'error',
+        'no-console': 'warn',
+        'prefer-const': 'error',
+        'no-var': 'error'
       }
     },
-    plugins: {
-      '@typescript-eslint': '@typescript-eslint/eslint-plugin'
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/prefer-const': 'error',
-      '@typescript-eslint/no-inferrable-types': 'error',
-      'no-console': 'warn',
-      'prefer-const': 'error',
-      'no-var': 'error'
+    {
+      files: ['tests/**/*.ts'],
+      rules: {
+        'no-console': 'off'
+      }
     }
-  },
-  {
-    files: ['tests/**/*.ts'],
-    rules: {
-      'no-console': 'off'
-    }
-  }
-];
+  ]
+};
